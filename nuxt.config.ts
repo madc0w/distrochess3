ce7690143107b7fb379f9aafb697a884bfb3cfac
Marc A. Donis <marc@balloonary.io>
2025-11-17 20:23:28 +0100

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
-import { defineNuxtConfig } from 'nuxt';
-
-export default defineNuxtConfig({
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default {
 	ssr: true,
 	runtimeConfig: {
-		mongodbUri: process.env.MONGODB_URI || '',
-		mongodbDb: process.env.MONGODB_DB || 'distrochess3',
+		mongodbUri: process.env.MONGODB_URI,
+		mongodbDb: process.env.MONGODB_DB,
 	},
 	typescript: { strict: true },
-});
+};
